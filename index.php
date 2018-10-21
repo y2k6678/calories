77079f2fe69de8121d54f498e9498edcdb6f120f
unknown <jojo_alejandro@hotmail.co.th>
2018-10-21 23:45:28 +0700

    fff

--- index.php
+++ index.php
@@ -81,10 +81,10 @@ if (!is_null($events['events']))
 				$qcount++;
 			}
 			$messages = ['type' => 'text',  'text' =>"รายการ : $qcount"];
-			if($qcount!=0)
-				$messages = ['type' => 'text',  'text' =>"รายการ : $foodname\nปริมาณ : $unit\nแคล : $cal"];
-			else
+			if($qcount==0)
 				$messages = ['type' => 'text',  'text' =>"รายการ : $foodname ไม่มีในระบบ"];
+			else
+				$messages = ['type' => 'text',  'text' =>"รายการ : $foodname ปริมาณ : $unit แคล : $cal"];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
