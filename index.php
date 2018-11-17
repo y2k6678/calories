c3d6937bfdfa0d7304169f30cb2978521a11de65
jojoniankoDev <jojo_alejandro@hotmail.co.th>
2018-11-17 18:45:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -118,7 +118,24 @@ try
 }
 catch(Exception  $ex)
 {
+	
 	echo $ex;
+	$messages = ['type' => 'text',  'text' =>"รายการ : $ex"];
+	$url = 'https://api.line.me/v2/bot/message/reply';
+				$data = ['replyToken' => $replyToken, 'messages' => [$messages], ];
+				$post = json_encode($data);
+				$headers = array(
+					'Content-Type: application/json',
+					'Authorization: Bearer ' . $access_token
+				);
+				$ch = curl_init($url);
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+				curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+				$result = curl_exec($ch);
+				curl_close($ch);
 }
 echo "OK";
 echo "ssssss";
